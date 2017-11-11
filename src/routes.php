c3f044fa23a30b15d4ba4a4aefd65437619e123b
Vladislav Kit <vladwebkit@gmail.com>
2017-11-11 11:00:16 +0200

    ok

--- src/routes.php
+++ src/routes.php
@@ -2,4 +2,4 @@
 
 Route::post('coinpayments/cancel', 'Selfreliance\CoinPayments\CoinPayments@cancel_payment')->name('coinpayments.cancel');
 Route::post('coinpayments/confirm', 'Selfreliance\CoinPayments\CoinPayments@validateIPNRequest')->name('coinpayments.confirm');
-// Route::post('coinpayments/webhookwithdraw', 'Selfreliance\CoinPayments\CoinPayments@check_transaction')->name('coinpayments.webhookwithdraw');
+Route::post('coinpayments/webhookwithdraw', 'Selfreliance\CoinPayments\CoinPayments@check_transaction')->name('coinpayments.webhookwithdraw');
